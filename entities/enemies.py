7cb0c3addf3c63c0c98a95abaf577fe7581c6802
GawarWawar <gawarwawar@gmail.com>
2024-06-19 00:08:55 +0300

    enemies running from entrance to base in straight line :D

--- entities/enemies.py
+++ entities/enemies.py
@@ -5,12 +5,23 @@ class Enemy(basic_tiles.Tile):
     
     def __init__(
         self,
-        health_points = 100,
+        coordinates,
+        width,
+        height,
+        
         fill_colour = (0, 0, 120),
-        border_size = 0,
+        border_size = 100,
+        corner_radius = (-1, -1, -1, -1),
+        
+        health_points = 100,
+        speed = 8,
+        
     ) -> None:
         self.health_points = 100
+        self.speed = 8
+        
         self.fill_colour = (0, 0, 120)
         self.border_size = 0
-        corner_radius = (-1, -1, -1, -1) #top_left, top_right, bottom_left, bottom_right
+        self.corner_radius = (-1, -1, -1, -1) #top_left, top_right, bottom_left, bottom_right
+        super().__init__(coordinates, width, height)
         
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import entities.basic_tiles as basic_tiles
 import entities.map_building as map_building
+import entities.enemies as enemies 
 
 pygame.init()
 WIDTH = 1280
@@ -18,10 +19,12 @@ dt = 0
 # for j in range(0, int(screen.get_height()), 20):
 #     lines[1].append(pygame.draw.line(screen, "white", pygame.Vector2(0, j), pygame.Vector2(screen.get_width(), j)))
 
-field = map_building.Field(300, 220, 20)
+field = map_building.Field(600, 440, 40)
 field.draw_field(screen)
 
-#draw a map
+# Draw a map example
+entrances :list[tuple[int, int]] = []
+bases :list[tuple[int, int]] = []
 for i in range(len(field.field_of_tiles)):
     if i == 0:
         tile_type = basic_tiles.Entrance
@@ -37,16 +40,42 @@ for i in range(len(field.field_of_tiles)):
         field.SQUARE_DIMENSIONS,
     )
     field.field_of_tiles[i][5].draw(screen)
+    
+    if tile_type == basic_tiles.Entrance:
+        entrances.append((i, 5))
+    if tile_type == basic_tiles.Base:
+        bases.append((i, 5))
 
 # for i in range(len(field)):
 #     print(field[i][0].topleft)
 
+# Stating conditionals
+ENEMY_WAVE = pygame.USEREVENT + 1
+pygame.time.set_timer(ENEMY_WAVE, 2000)
 
+foes : list[enemies.Enemy] = []
+
+# Main game loop
 while running:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == ENEMY_WAVE:
+            for i in range(1):
+                foe_coordinates = field.field_of_tiles[entrances[0][0]][entrances[0][1]].rect.topleft
+                print(foe_coordinates)
+                foe_to_append = (
+                    enemies.Enemy(
+                        foe_coordinates,
+                        field.SQUARE_DIMENSIONS,
+                        field.SQUARE_DIMENSIONS
+                    )
+                )
+                foe_to_append.draw(screen)
+                print(foe_to_append.rect.topleft)
+                foes.append(foe_to_append)
         
         #player actions
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -58,18 +87,11 @@ while running:
                 
                 tile_arr_pos = field.get_square_array_position(mouse_position)
                 tile_coordinates = field.field_of_tiles[tile_arr_pos[0]][tile_arr_pos[1]].rect.topleft
-                
                     
-
-                
                 if not isinstance(
                     field.field_of_tiles[tile_arr_pos[0]][tile_arr_pos[1]],
                     basic_tiles.Road
                 ):
-                    print(isinstance(
-                    field.field_of_tiles[tile_arr_pos[0]][tile_arr_pos[1]],
-                    basic_tiles.TowerFundament
-                ))
                     if isinstance(
                         field.field_of_tiles[tile_arr_pos[0]][tile_arr_pos[1]],
                         basic_tiles.TowerFundament
@@ -89,7 +111,18 @@ while running:
                                 field.SQUARE_DIMENSIONS,
                             )
                         field.field_of_tiles[tile_arr_pos[0]][tile_arr_pos[1]].draw(screen)
-                    
+        
+    for row in field.field_of_tiles:
+        for tile in row: 
+            tile.draw(screen)    
+        
+    for foe_arr_pos, foe in enumerate(foes):
+        foes[foe_arr_pos].rect = foe.rect.move(foe.speed, 0)      
+        pygame.draw.rect(screen, foes[foe_arr_pos].fill_colour, foes[foe_arr_pos].rect)
+        if foe.rect.colliderect(
+            field.field_of_tiles[bases[0][0]][bases[0][1]].rect
+        ):
+            foes.pop(foe_arr_pos)
             
             
     if pygame.mouse.get_pressed()[0]:
