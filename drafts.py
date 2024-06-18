095a5b77d62acca710baa721e63b379028a0e64a
GawarWawar <gawarwawar@gmail.com>
2024-06-18 22:53:00 +0300

    added file for drafts that i can use in future

---
+++ drafts.py
@@ -0,0 +1,8 @@
+# Button functionality
+    # action == False
+    # mouse_position = pygame.mouse.get_pos()
+    # if self.rect.collidepoint(mouse_position):
+    #     if pygame.mouse.get_pressed()[0] == 1:
+    #         self.clicked = True
+    #         action = True
+    # return action 
