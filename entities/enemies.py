3753bcfe8cdedcf7ee35c6cbb322d6d952aaa832
GawarWawar <gawarwawar@gmail.com>
2024-06-20 18:38:36 +0300

    fixed properties assign process for enemies

--- entities/enemies.py
+++ entities/enemies.py
@@ -11,17 +11,17 @@ class Enemy(basic_tiles.Tile):
         
         fill_colour = (0, 0, 120),
         border_size = 100,
-        corner_radius = (-1, -1, -1, -1),
+        corner_radius = (-1, -1, -1, -1), # top_left, top_right, bottom_left, bottom_right
         
         health_points = 100,
-        speed = 8,
+        speed = 3,
         
     ) -> None:
-        self.health_points = 100
-        self.speed = 3
+        self.health_points = health_points
+        self.speed = speed
         
-        self.fill_colour = (0, 0, 120)
-        self.border_size = 0
-        self.corner_radius = (-1, -1, -1, -1) #top_left, top_right, bottom_left, bottom_right
+        self.fill_colour = fill_colour
+        self.border_size = border_size
+        self.corner_radius = corner_radius 
         super().__init__(coordinates, width, height)
         
